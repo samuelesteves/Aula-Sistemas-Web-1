ccd08d7b89639a889e8cd842383e858723d613d7
samuel <saamuelsiilva215@gmail.com>
2022-03-15 10:09:33 -0300

    Adição do código 'teste'

---
+++ teste/teste/Context/Contexto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace teste.Context
+{
+    public class Contexto : DbContext
+    {
+        public Contexto() : base("DBteste")
+        {
+
+        }
+        
+        public DbSet<AlunoModel> Aluno { get; set; }
+    }
+}
---
+++ teste/teste/Migrations/202203151230358_initial.cs
@@ -0,0 +1,30 @@
+﻿namespace teste.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.AlunoModels",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Nome = c.String(nullable: false),
+                        Curso = c.String(nullable: false),
+                        Telefone = c.String(),
+                        Email = c.String(),
+                        Sexo = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.AlunoModels");
+        }
+    }
+}
